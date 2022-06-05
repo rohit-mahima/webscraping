84613568dd3a566a3c1feff3fb053bd1218fed37
rranja33 <rohitranjanmahima@gmail.com>
2022-06-06 02:25:24 +0530

    commit to new branhc

--- projects/context_manager.py
+++ projects/context_manager.py
@@ -1,5 +1,17 @@
 #context manager
+import sqlite3
+
+
 class Conn_Open:
+    def __init__(self,db_name):
+        self.db_name=db_name
+
+    def __enter__(self):
+        self.conn=sqlite3.connect(self.db_name)
+        return self.conn
+
+    def __exit__
+
 
 
 if __name__=='__main__':
