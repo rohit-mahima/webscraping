9db952626c77d8fab8406d3b46a0dc9c6762509a
rranja33 <rohitranjanmahima@gmail.com>
2022-06-06 10:58:18 +0530

    improving the test cases

--- projects/testmymath.py
+++ projects/testmymath.py
@@ -5,19 +5,19 @@ class TestAdd(unittest.TestCase):
     """ 
     Test the additon function 
     """
-    def add_integer(self):
+    def test_add_integer(self):# it is importatnt to write test
         """ 
         Test the addition of two numbers 
         """
         result=mymath.add(1, 2)
         self.assertEqual(result,3)
 
-    def add_float(self):
+    def test_add_float(self):
         """ Test the additon of float values """
         result=mymath.add(10.5,4.5)
         self.assertEqual(result,15)
 
-    def add_string(self):
+    def test_add_string(self):
         """ Test the addtion of stirngs """
         result=mymath.add('abc','def')
         self.assertEqual(result,'abcdef')
